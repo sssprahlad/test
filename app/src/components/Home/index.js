724c4a7a182befb19675ff10d163525a36a34663
sssprahlad <k.sssprahlad@gmail.com>
2024-06-12 15:31:56 +0530

    upload

---
+++ app/src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <p>Conditional redering</p>
+      <button>Click</button>
+    </div>
+  );
+};
+
+export default index;
